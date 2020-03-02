25cdba270a08185fff46d8aa6f4efb9735645878
Aayush Fadia <aayushf@gmail.com>
2020-03-02 17:25:01 +0530

    Extract URL and print at every navigation event. Also uncovered security hole.

--- web_renderer.py
+++ web_renderer.py
@@ -2,28 +2,29 @@
 import wx
 import wx.html2
 from screeninfo import get_monitors
+
 m = get_monitors()[0]
 HEIGHT = m.height
 WIDTH = m.width
 
 
-def printUrl(event):
-  print("Navigated")
-
-
 class MyBrowser(wx.Frame):
-  def __init__(self, *args, **kwds):
-    wx.Frame.__init__(self, *args, **kwds)
-    sizer = wx.BoxSizer(wx.VERTICAL)
-    self.browser = wx.html2.WebView.New(self)
-    sizer.Add(self.browser, 1, wx.EXPAND, 10)
-    self.SetSizer(sizer)
-    self.SetSize((WIDTH/2, HEIGHT/2))
-    self.Bind(wx.EVT_MOTION, printUrl)
+    def printUrl(self, event):
+        print(event.URL)
+
+    def __init__(self, *args, **kwds):
+        wx.Frame.__init__(self, *args, **kwds)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        self.browser = wx.html2.WebView.New(self)
+        sizer.Add(self.browser, 1, wx.EXPAND, 10)
+        self.SetSizer(sizer)
+        self.SetSize((WIDTH / 2, HEIGHT / 2))
+        self.browser.Bind(wx.html2.EVT_WEBVIEW_NAVIGATING, self.printUrl)
+
 
 if __name__ == '__main__':
-  app = wx.App()
-  dialog = MyBrowser(None, -1)
-  dialog.browser.LoadURL("https://www.greatlearning.in/")
-  dialog.Show()
-  app.MainLoop()
+    app = wx.App()
+    dialog = MyBrowser(None, -1)
+    dialog.browser.LoadURL("https://www.greatlearning.in/")
+    dialog.Show()
+    app.MainLoop()
