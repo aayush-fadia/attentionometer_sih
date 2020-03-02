bbe2e5eaa26632c7256883a6a06e6697c9edcbae
Aayush Fadia <aayushf@gmail.com>
2020-03-02 16:56:29 +0530

    Website now occupies one fourth of screen. Changed from dialog to frame, so we get window titlebars and borders.

--- web_renderer.py
+++ web_renderer.py
@@ -1,15 +1,25 @@
 # run 'pip install wxpython' before running this code. This will take time to complete, don't panic.
 import wx
 import wx.html2
+from screeninfo import get_monitors
+m = get_monitors()[0]
+HEIGHT = m.height
+WIDTH = m.width
 
-class MyBrowser(wx.Dialog):
+
+def printUrl(event):
+  print("Navigated")
+
+
+class MyBrowser(wx.Frame):
   def __init__(self, *args, **kwds):
-    wx.Dialog.__init__(self, *args, **kwds)
+    wx.Frame.__init__(self, *args, **kwds)
     sizer = wx.BoxSizer(wx.VERTICAL)
     self.browser = wx.html2.WebView.New(self)
     sizer.Add(self.browser, 1, wx.EXPAND, 10)
     self.SetSizer(sizer)
-    self.SetSize((1920, 1080))
+    self.SetSize((WIDTH/2, HEIGHT/2))
+    self.Bind(wx.EVT_MOTION, printUrl)
 
 if __name__ == '__main__':
   app = wx.App()
