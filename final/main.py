32b2085a20fd2836e6675a5d0a7c86cce16530ac
Aayush Fadia <aayushf@gmail.com>
2020-04-28 17:19:58 +0530

    Cleaned up code, used deque with maxlen.

--- final/main.py
+++ final/main.py
@@ -1,16 +1,16 @@
+from collections import deque
+
 import cv2
 
-from collections import deque
 from face_detect_dlib import get_face_rect_dlib
 from face_keypoint_predict_dlib import get_face_keypoints
-from gaze_detector import predict_column
 
 FILL_MEMORY = False
 EVERY_N_FRAMES = 20
 NUM_FRAMES_PASSED = 0
 NUM_FRAMES_COLLECTED = 0
 TOTAL_FRAMES = 48
-CONSEC_FRAMES = deque()
+CONSEC_FRAMES = deque(maxlen=TOTAL_FRAMES)
 is_eye_closed = 0
 is_eye_closed_weight = 1.0
 lip_variance = 0.0
@@ -38,16 +38,9 @@ while cap.isOpened():
     if NUM_FRAMES_PASSED == EVERY_N_FRAMES:
         ## eye_gaze_variance = CALL EYE GAZE VARIANCE ##
         NUM_FRAMES_PASSED = 0
-
-    if FILL_MEMORY is False:
-        CONSEC_FRAMES.append(face_keypoints)
-        NUM_FRAMES_COLLECTED += 1
-        if NUM_FRAMES_COLLECTED == TOTAL_FRAMES:
-            FILL_MEMORY = True
-    else:
+    CONSEC_FRAMES.append(face_keypoints)
+    if len(CONSEC_FRAMES) == TOTAL_FRAMES:
+        pass
         ## is_eye_closed = CALL EYE CLOSURE (list(CONSEC_FRAMES)) ##
         ## lip_variance = CALL LIP VARIANCE (list(CONSEC_FRAMES)) ##
-        CONSEC_FRAMES.popleft()
-        CONSEC_FRAMES.append(face_keypoints)
-
     show_output(frame)
