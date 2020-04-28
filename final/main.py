3790dead6bf0551b595a333d9e0039e0c8a0fc8a
saum7800 <gandhisaumya8@gmail.com>
2020-04-28 16:52:27 +0530

    process eye closure and lip variance every frame. also flipped frame

--- final/main.py
+++ final/main.py
@@ -1,13 +1,15 @@
 import cv2
 
+from collections import deque
 from face_detect_dlib import get_face_rect_dlib
 from face_keypoint_predict_dlib import get_face_keypoints, draw_face_keypoints
 
+FILL_MEMORY = False
 EVERY_N_FRAMES = 20
 NUM_FRAMES_PASSED = 0
 NUM_FRAMES_COLLECTED = 0
 TOTAL_FRAMES = 48
-CONSEC_FRAMES = []
+CONSEC_FRAMES = deque()
 is_eye_closed = 0
 is_eye_closed_weight = 1.0
 lip_variance = 0.0
@@ -24,6 +26,7 @@ def show_output(frame):
 cap = cv2.VideoCapture(0)
 while cap.isOpened():
     ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
     face_rect = get_face_rect_dlib(frame)
     if face_rect is None:
         print("No Face Found!")
@@ -36,13 +39,15 @@ while cap.isOpened():
         ## eye_gaze_variance = CALL EYE GAZE VARIANCE ##
         NUM_FRAMES_PASSED = 0
 
-    if NUM_FRAMES_COLLECTED < TOTAL_FRAMES:
+    if FILL_MEMORY is False:
         CONSEC_FRAMES.append(face_keypoints)
         NUM_FRAMES_COLLECTED += 1
+        if NUM_FRAMES_COLLECTED == TOTAL_FRAMES:
+            FILL_MEMORY = True
     else:
-        ## is_eye_closed = CALL EYE CLOSURE (CONSEC_FRAMES) ##
-        ## lip_variance = CALL LIP VARIANCE (CONSEC_FRAMES) ##
-        CONSEC_FRAMES = []
-        NUM_FRAMES_COLLECTED = 0
+        ## is_eye_closed = CALL EYE CLOSURE (list(CONSEC_FRAMES)) ##
+        ## lip_variance = CALL LIP VARIANCE (list(CONSEC_FRAMES)) ##
+        CONSEC_FRAMES.popleft()
+        CONSEC_FRAMES.append(face_keypoints)
 
     show_output(frame)
