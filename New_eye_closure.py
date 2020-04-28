d357f3c292e436a1f34584a817e2b2b5a0b891ff
KanishkaSoni <54025453+KanishkaSoni@users.noreply.github.com>
2020-04-28 16:18:36 +0530

    Updated eye Closure

--- New_eye_closure.py
+++ New_eye_closure.py
@@ -33,59 +33,33 @@ def eye_aspect_ratio(eye):
     # return the eye aspect ratio
     return ear
 #VideoFeed contains the 48 frames
-def Is_eye_open(VideoFeed):
-    ans=1
+def Is_eye_closed(VideoFeed):
+    ans=0
     # grab the indexes of the facial landmarks for the left and
     # right eye, respectively
     (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
     (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
-    lookUpTable = np.empty((1, 256), np.uint8)
-    gamma = 0.5
-    for i in range(256):
-        lookUpTable[0, i] = np.clip(pow(i / 255.0, gamma) * 255.0, 0, 255)
-
     for i in range(EYE_AR_CONSEC_FRAMES):
-        frame = VideoFeed(i)
-        frame = imutils.resize(frame, width=450)
-        frame = cv2.LUT(frame, lookUpTable)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # detect faces in the grayscale frame
-        rects = detector(gray, 0)
-        if len(rects) > 0:
-            for rect in rects:
-                # determine the facial landmarks for the face region, then
-                # convert the facial landmark (x, y)-coordinates to a NumPy
-                # array
-                shape = predictor(gray, rect)
-                shape = face_utils.shape_to_np(shape)
-                # extract the left and right eye coordinates, then use the
-                # coordinates to compute the eye aspect ratio for both eyes
-                leftEye = shape[lStart:lEnd]
-                rightEye = shape[rStart:rEnd]
-                leftEAR = eye_aspect_ratio(leftEye)
-                rightEAR = eye_aspect_ratio(rightEye)
-                # average the eye aspect ratio together for both eyes
-                ear = (leftEAR + rightEAR) / 2.0
-                # compute the convex hull for the left and right eye, then
-                # visualize each of the eyes
-                #leftEyeHull = cv2.convexHull(leftEye)
-                #rightEyeHull = cv2.convexHull(rightEye)
-                #cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-                #cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        shape = VideoFeed(i)
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+        # average the eye aspect ratio together for both eyes
+        ear = (leftEAR + rightEAR) / 2.0
+    
 
-                # check to see if the eye aspect ratio is below the blink
-                # threshold, and if so, increment the blink frame counter
-                if ear < EYE_AR_THRESH:
-                    COUNTER += 1
-                    # if the eyes were closed for a sufficient number of
-                    # then sound the alarm
-                    if COUNTER >= EYE_AR_CONSEC_FRAMES:
-                        ans=0
-                        break
-                # otherwise, the eye aspect ratio is not below the blink
-                # threshold, so reset the counter and alarm
-                else:
-                    COUNTER = 0
+        # check to see if the eye aspect ratio is below the blink
+        # threshold, and if so, increment the blink frame counter
+        if ear < EYE_AR_THRESH:
+            COUNTER += 1
+            if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                ans=1
+                break
+        # otherwise, the eye aspect ratio is not below the blink
+        # threshold, so reset the counter and alarm
+        else:
+            COUNTER = 0
 
 
         # show the frame
