c76fe8e9b951f07078f7fa9d9be797a4d27b2b31
saum7800 <gandhisaumya8@gmail.com>
2020-08-01 00:42:21 +0530

    changed name and variance returns

--- final2/executor.py
+++ final2/executor.py
@@ -1,5 +1,5 @@
 from landmarks3d import get_face_keypoints, calculate_attention
-from extract_name import get_name
+#from extract_name import get_name
 from lip_var import get_lip_var2
 from collections import defaultdict, deque
 from multiprocessing import RLock
@@ -8,13 +8,21 @@ lock = RLock()
 buffer = defaultdict(lambda: deque(maxlen=48))
 
 
-def process_and_upload(frame):
+def process_and_upload(enumframes):
+    i, frame = enumframes
     print("Processing one frame")
-    name = get_name(frame)
+    #name = get_name(frame)
+    name = str(i)
     print("NAME IS {}".format(name))
     landmarks = get_face_keypoints(frame)
     cur_attention = calculate_attention(landmarks)
     variance, distance = get_lip_var2(buffer[name], landmarks)
-    with lock:
-        buffer[name].append(distance)
+    #print("Attention", cur_attention)
+    #print("lip var", 0.001*variance)
+    if distance != -1:
+        with lock:
+            buffer[name].append(distance)
+    if variance!=-1:
+        cur_attention+=(0.001*variance)
+
     print("UPLOAD {} FOR {}".format(cur_attention, name))
